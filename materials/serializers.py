4c1bd3234ef679e98b0bb0315e7fbe545de986a7
Valentina Ovsianik <forovp@gmail.com>
2024-12-10 22:54:06 +0300

    Добавлены сериализаторы

---
+++ materials/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Course, Lesson
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для курса"""
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    """Сериализатор для урока"""
+    class Meta:
+        model = Lesson
+        fields = "__all__"
