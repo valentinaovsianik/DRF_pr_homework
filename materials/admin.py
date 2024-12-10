f3e9f67865ed3e855abd3c5ebf08a8c5536fc0bf
Valentina Ovsianik <forovp@gmail.com>
2024-12-10 22:57:05 +0300

    Модели зарегистрированы в админке

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "course")
+    search_fields = ("title", "course__title")
+    list_filter = ("course",)
