941aadd4f79594de6fc496a9bd4cd479ce15773b
Valentina Ovsianik <forovp@gmail.com>
2024-12-11 20:57:15 +0300

    Создана модель платежа, применены миграции

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="Email")  # Авторизация по email
@@ -18,3 +20,42 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    """Модель платежа"""
+
+    CASH = "cash"
+    TRANSFER = "transfer"
+    PAYMENT_METHODS = [
+        (CASH, "Наличные"),
+        (TRANSFER, "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты")
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        related_name="course_payments",
+        verbose_name="Оплаченный курс",
+        blank=True,
+        null=True,
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.SET_NULL,
+        related_name="lesson_payments",
+        verbose_name="Оплаченный урок",
+        blank=True,
+        null=True,
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS, verbose_name="Способ оплаты")
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f"Платеж от {self.user.email} ({self.amount} руб.)"
