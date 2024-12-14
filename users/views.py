ac45b19bb55d6c50b9af4d5cc61f94a018b0756f
Valentina Ovsianik <forovp@gmail.com>
2024-12-14 21:01:46 +0300

    Настроена фильтрация для списка платежей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import User, Payment
+from .serializers import UserSerializer, PaymentSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PaymentsViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+
+    filterset_fields = (
+        "course",
+        "lesson",
+        "payment_method",
+    )
+    ordering_fields = ["payment_date",]
