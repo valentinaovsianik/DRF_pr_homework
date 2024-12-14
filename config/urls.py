2be7840de85bd6ba38ec37246745ee0c7129686f
Valentina Ovsianik <forovp@gmail.com>
2024-12-14 21:05:39 +0300

    Внесены исправления

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("users/", include("users.urls", namespace="users")),
+    path("users/", include("users.urls", namespace="users")),
     path("materials/", include("materials.urls", namespace="materials")),
 ]
