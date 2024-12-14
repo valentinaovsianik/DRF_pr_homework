6c2df4a88f8c38a5d8eb9d7ed14a5f1124d312c0
Valentina Ovsianik <forovp@gmail.com>
2024-12-14 19:42:24 +0300

    Для сериализатора для модели курса реализовано поле вывода уроков

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,22 +3,25 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для курса"""
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-    def get_lessons_count(self, obj):
-        """Возвращает количество уроков курса"""
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для урока"""
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для курса"""
+
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, obj):
+        """Возвращает количество уроков курса"""
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = "__all__"
+
