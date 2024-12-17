9240bae84a66ca20a6f8218cc732972b1855b4f5
Valentina Ovsianik <forovp@gmail.com>
2024-12-17 21:17:06 +0300

    Создана группа Moderator

---
+++ users/management/commands/create_groups.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+
+class Command(BaseCommand):
+    """Создает группу Moderator"""
+
+    def handle(self, *args, **kwargs):
+        group, created = Group.objects.get_or_create(name="Moderator")
+        if created:
+            self.stdout.write(self.style.SUCCESS("Группа Moderator создана"))
+        else:
+            self.stdout.write(self.style.WARNING("Группа Moderator уже существует"))
