eeb54a9b045098388c6cf3f6abec4d60559a808c
Valentina Ovsianik <forovp@gmail.com>
2024-12-24 20:30:48 +0300

    Реализована возможность для установки/удаления подписки пользователя на обновления курса

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,17 +1,16 @@
 from rest_framework import serializers
 
-from .models import Course, Lesson
-
+from .models import Course, Lesson, Subscription
 from .validators import YouTubeURLValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для урока"""
+    video_url = serializers.URLField(validators=[YouTubeURLValidator()])
 
     class Meta:
         model = Lesson
         fields = "__all__"
-        validators = [YouTubeURLValidator(field="video_url")]
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -19,10 +18,16 @@ class CourseSerializer(serializers.ModelSerializer):
 
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    is_subscription = serializers.SerializerMethodField()
 
     def get_lessons_count(self, obj):
         """Возвращает количество уроков курса"""
         return obj.lessons.count()
+
+    def get_is_subscription(self, obj):
+        """Проверяет наличие подписки на курс"""
+        user = self.context["request"].user
+        return Subscription.objects.filter(user=user, course=obj).exists()
 
     class Meta:
         model = Course
--- materials/views.py
+++ materials/views.py
@@ -1,10 +1,14 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from users.permissions import IsModerator, IsOwner
 
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscription
 from .serializers import CourseSerializer, LessonSerializer
 
 
@@ -58,3 +62,24 @@ class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated, IsOwner | ~IsModerator]
+
+
+class SubscriptionView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        course_id = request.data.get("course_id")
+        course_item = get_object_or_404(Course, id=course_id)
+
+        # Получаем объекты подписок по текущему пользователю и курса
+        subs_item = Subscription.objects.filter(user=user, course=course_item)
+
+        if subs_item.exists():  # Если подписка уже существует, удаляем ее
+            subs_item.delete()
+            message = "Подписка удалена."
+        else:  # Если подписки нет, создаем ее
+            Subscription.objects.create(user=user, course=course_item)
+            message = "Подписка добавлена."
+
+        return Response({"message": message}, status=status.HTTP_200_OK)
