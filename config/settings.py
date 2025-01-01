cafdb9f3d88c3d7f03469ac0cc3e7b27e21b0eb8
Valentina Ovsianik <forovp@gmail.com>
2025-01-01 15:41:52 +0300

    Реализована возможность оплаты курсов

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
